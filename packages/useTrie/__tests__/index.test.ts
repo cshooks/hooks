2a23f74d6f8ae6e4b2257357571bdf9587f3fd9a
dance2die <sungmkim80@gmail.com>
2019-03-05 14:14:03 -0500

    Case Insensitive tests all pass

--- packages/useTrie/__tests__/index.test.ts
+++ packages/useTrie/__tests__/index.test.ts
@@ -48,6 +48,57 @@ describe("Case Insensitive Tests", () => {
     expect(trie.has("d", false)).toBe(false);
     expect(trie.has("", false)).toBe(false);
   });
+
+  test("Add new terms and confirm that it exists", () => {
+    const trie = new Trie([], isCaseSensitive);
+    trie.add("abc");
+    trie.add("abd");
+
+    expect(trie.has("abc")).toBe(true);
+    expect(trie.has("abd")).toBe(true);
+    expect(trie.has("abx")).toBe(false);
+    expect(trie.has("dex")).toBe(false);
+    expect(trie.has("")).toBe(false);
+    expect(trie.has("a", false)).toBe(true);
+    expect(trie.has("d", false)).toBe(false);
+    expect(trie.has("", false)).toBe(false);
+  });
+
+  test("Trying to delete from an empty trie should not fail", () => {
+    const trie = new Trie([], isCaseSensitive);
+    expect(() => trie.remove("aaa")).not.toThrow();
+  });
+
+  test("Remove terms and confirm that it does not exist", () => {
+    const trie = new Trie(["abcd", "abce"], isCaseSensitive);
+    // As the case is "in"-sensitive, removing capitalized word should work too.
+    trie.remove("ABCD");
+    expect(trie.has("abcd")).toBe(false);
+    expect(trie.has("a", false)).toBe(true);
+    expect(trie.has("ab", false)).toBe(true);
+    expect(trie.has("abc", false)).toBe(true);
+    expect(trie.has("abce", false)).toBe(true);
+    expect(trie.has("abce")).toBe(true);
+
+    trie.remove("abce");
+    expect(trie.has("abce")).toBe(false);
+    expect(trie.isEmpty()).toBe(true);
+
+    trie.remove("abcd");
+    expect(trie.has("abcd")).toBe(false);
+    expect(trie.has("abda")).toBe(false);
+  });
+
+  test("Check if trie is empty or not", () => {
+    const trie = new Trie([], isCaseSensitive);
+    expect(trie.isEmpty()).toBe(true);
+
+    trie.add("ab");
+    expect(trie.isEmpty()).toBe(false);
+
+    trie.remove("ab");
+    expect(trie.isEmpty()).toBe(true);
+  });
 });
 
 describe("Case Sensitive Tests", () => {
