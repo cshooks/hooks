7d6f5fa27dd2a78da863e7d1bb67ebc1714b9439
dance2die <sungmkim80@gmail.com>
2019-03-05 13:24:13 -0500

    Added 'describe' block to group case insensitive tests

--- packages/useTrie/__tests__/index.test.ts
+++ packages/useTrie/__tests__/index.test.ts
@@ -1,75 +1,71 @@
 "use strict";
 
-// const useTrie = require('..');
-
-// describe('@cshooks/useTrie', () => {
-//     it('needs tests');
-// });
-
 import { Trie } from "../src/index";
 
-test("Trie has an exact search term", () => {
-  const trie = new Trie(["abc", "abd"]);
-  expect(trie.has("abc")).toBe(true);
-  expect(trie.has("abd")).toBe(true);
-  expect(trie.has("abx")).toBe(false);
-  expect(trie.has("dex")).toBe(false);
-  expect(trie.has("")).toBe(false);
-});
+describe("Case Insensitive Tests", () => {
+  test("Trie has an exact search term", () => {
+    const trie = new Trie(["abc", "abd"]);
+    expect(trie.has("abc")).toBe(true);
+    expect(trie.has("abd")).toBe(true);
+    expect(trie.has("abx")).toBe(false);
+    expect(trie.has("dex")).toBe(false);
+    expect(trie.has("")).toBe(false);
+  });
 
-test("Trie has a fuzz search term", () => {
-  const trie = new Trie(["abcd", "abda"]);
-  expect(trie.has("a", false)).toBe(true);
-  expect(trie.has("d", false)).toBe(false);
-  expect(trie.has("", false)).toBe(false);
-});
+  test("Trie has a fuzz search term", () => {
+    const trie = new Trie(["abcd", "abda"]);
+    expect(trie.has("a", false)).toBe(true);
+    expect(trie.has("d", false)).toBe(false);
+    expect(trie.has("", false)).toBe(false);
+  });
 
-test("Add new terms and confirm that it exists", () => {
-  const trie = new Trie([]);
-  trie.add("abc");
-  trie.add("abd");
+  test("Add new terms and confirm that it exists", () => {
+    const trie = new Trie([]);
+    trie.add("abc");
+    trie.add("abd");
 
-  expect(trie.has("abc")).toBe(true);
-  expect(trie.has("abd")).toBe(true);
-  expect(trie.has("abx")).toBe(false);
-  expect(trie.has("dex")).toBe(false);
-  expect(trie.has("")).toBe(false);
-  expect(trie.has("a", false)).toBe(true);
-  expect(trie.has("d", false)).toBe(false);
-  expect(trie.has("", false)).toBe(false);
-});
+    expect(trie.has("abc")).toBe(true);
+    expect(trie.has("abd")).toBe(true);
+    expect(trie.has("abx")).toBe(false);
+    expect(trie.has("dex")).toBe(false);
+    expect(trie.has("")).toBe(false);
+    expect(trie.has("a", false)).toBe(true);
+    expect(trie.has("d", false)).toBe(false);
+    expect(trie.has("", false)).toBe(false);
+  });
 
-test("Trying to delete from an empty trie should not fail", () => {
-  const trie = new Trie([]);
-  expect(() => trie.remove("aaa")).not.toThrow();
-});
+  test("Trying to delete from an empty trie should not fail", () => {
+    const trie = new Trie([]);
+    expect(() => trie.remove("aaa")).not.toThrow();
+  });
 
-test("Remove terms and confirm that it does not exist", () => {
-  const trie = new Trie(["abcd", "abce"]);
-  trie.remove("abcd");
-  expect(trie.has("abcd")).toBe(false);
-  expect(trie.has("a", false)).toBe(true);
-  expect(trie.has("ab", false)).toBe(true);
-  expect(trie.has("abc", false)).toBe(true);
-  expect(trie.has("abce", false)).toBe(true);
-  expect(trie.has("abce")).toBe(true);
+  test("Remove terms and confirm that it does not exist", () => {
+    const trie = new Trie(["abcd", "abce"]);
+    trie.remove("abcd");
+    expect(trie.has("abcd")).toBe(false);
+    expect(trie.has("a", false)).toBe(true);
+    expect(trie.has("ab", false)).toBe(true);
+    expect(trie.has("abc", false)).toBe(true);
+    expect(trie.has("abce", false)).toBe(true);
+    expect(trie.has("abce")).toBe(true);
 
-  trie.remove("abce");
-  expect(trie.has("abce")).toBe(false);
-  expect(trie.isEmpty()).toBe(true);
+    trie.remove("abce");
+    expect(trie.has("abce")).toBe(false);
+    expect(trie.isEmpty()).toBe(true);
 
-  trie.remove("abcd");
-  expect(trie.has("abcd")).toBe(false);
-  expect(trie.has("abda")).toBe(false);
-});
+    trie.remove("abcd");
+    expect(trie.has("abcd")).toBe(false);
+    expect(trie.has("abda")).toBe(false);
+  });
 
-test("Check if trie is empty or not", () => {
-  const trie = new Trie([]);
-  expect(trie.isEmpty()).toBe(true);
+  test("Check if trie is empty or not", () => {
+    const trie = new Trie([]);
+    expect(trie.isEmpty()).toBe(true);
 
-  trie.add("ab");
-  expect(trie.isEmpty()).toBe(false);
+    trie.add("ab");
+    expect(trie.isEmpty()).toBe(false);
 
-  trie.remove("ab");
-  expect(trie.isEmpty()).toBe(true);
+    trie.remove("ab");
+    expect(trie.isEmpty()).toBe(true);
+  });
 });
