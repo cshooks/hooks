2bef4d7f5cb2debde2400eed81834cafd5199db0
dance2die <sungmkim80@gmail.com>
2019-03-06 21:03:04 -0500

    Turned on jest test coverage option

--- packages/useTrie/jest.config.js
+++ packages/useTrie/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
   },
   testRegex: "(/__tests__/.*|(\\.|/)(test|spec))\\.tsx?$",
   // testRegex: "(/__tests__/**/*.ts?(x),**/?(*.)+(spec|test).ts?(x)",
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"]
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
   // https://medium.com/@mtiller/debugging-with-typescript-jest-ts-jest-and-visual-studio-code-ef9ca8644132
-  // collectCoverage: true
+  collectCoverage: true
 };
