27b0a5852be12bcca1ee9358fd6a778563d904b8
Nick Taylor <nick@iamdeveloper.com>
2019-03-25 23:24:06 -0400

    Not necessary to have methods as properties using fat arrow.

--- packages/useTrie/src/index.ts
+++ packages/useTrie/src/index.ts
@@ -57,8 +57,8 @@ class Trie implements ITrie {
     words.forEach(word => this.add(word, this.getText));
   }
 
-  private normalizeWord = (word: Word) =>
-    this.isCaseInsensitive
+  private normalizeWord(word: Word): string {
+    return this.isCaseInsensitive
       ? (typeof word === 'string'
           ? word
           : this.getText(word) || ''
@@ -66,12 +66,13 @@ class Trie implements ITrie {
       : typeof word === 'string'
       ? word
       : this.getText(word);
+  }
 
   /*
    * @param {string} word A word to check if it exists in the trie
    * @param {boolean} exactSearch Return true only if the exact word is stored
    */
-  has = (wordToSearch: string, exactSearch: boolean = true): boolean => {
+  has(wordToSearch: string, exactSearch: boolean = true): boolean {
     let word = this.normalizeWord(wordToSearch);
     if (word === '') return false;
 
@@ -86,9 +87,9 @@ class Trie implements ITrie {
 
     // If "id" at the current node exists, then it's a word
     return exactSearch ? !!head.id : true;
-  };
+  }
 
-  add = (wordToAdd: Word, getText: TextSelector = obj => obj): void => {
+  add(wordToAdd: Word, getText: TextSelector = obj => obj): void {
     this.getText = this.getText || getText;
 
     let word = this.normalizeWord(wordToAdd);
@@ -104,15 +105,15 @@ class Trie implements ITrie {
     }
 
     head.id = wordToAdd;
-  };
+  }
 
   // https://www.geeksforgeeks.org/trie-delete/
-  remove = (wordToRemove: string): void => {
+  remove(wordToRemove: string): void {
     let word = this.normalizeWord(wordToRemove);
     if (this.isEmpty() || !this.has(word)) return;
 
     this.root = this._remove(this.root, word);
-  };
+  }
 
   private _remove(node: ITrieNode, word: string, depth: number = 0): ITrieNode {
     if (!node) return new TrieNode();
@@ -136,15 +137,16 @@ class Trie implements ITrie {
     return node;
   }
 
-  private _isEmpty = (root: ITrieNode = this.root): boolean => {
+  private _isEmpty(root: ITrieNode = this.root): boolean {
     return Object.keys(root.next).length === 0;
-  };
+  }
   isEmpty = (): boolean => {
     return this._isEmpty(this.root);
   };
 
-  private isLastNode = (node: ITrieNode): boolean =>
-    Object.keys(node.next).length === 0;
+  private isLastNode(node: ITrieNode): boolean {
+    return Object.keys(node.next).length === 0;
+  }
 
   private traverseToChildren(
     node: ITrieNode,
@@ -161,7 +163,7 @@ class Trie implements ITrie {
   }
 
   // https://www.geeksforgeeks.org/auto-complete-feature-using-trie/
-  search = (wordToSearch: string): Word[] => {
+  search(wordToSearch: string): Word[] {
     const word = this.normalizeWord(wordToSearch);
     const children = this.traverseToChildren(this.root, word, 0);
 
@@ -170,7 +172,7 @@ class Trie implements ITrie {
     // the Root contains the last letter in the search term
     this.searchChildren(children, '', word, word.length, acc);
     return acc;
-  };
+  }
 
   private searchChildren(
     root: ITrieNode,
