fb0d4714a387c76127924a23ee3f92c4335df15e
dance2die <sungmkim80@gmail.com>
2019-04-03 10:50:09 -0400

    Migrated source from https://github.com/dance2die/throwaway.useheap

--- packages/useHeap/src/index.ts
+++ packages/useHeap/src/index.ts
@@ -1,6 +1,169 @@
-export const sum = (a: number, b: number) => {
-  if ('development' === process.env.NODE_ENV) {
-    console.log('fuck');
+import React from "react";
+
+// const log = console.log;
+
+type Value = number;
+const enum ActionType {
+  Add = "ADD",
+  Set = "SET",
+  Remove = "REMOVE",
+  Get = "GET"
+}
+
+type Action =
+  | { type: ActionType.Add; payload: { value: Value } }
+  | { type: ActionType.Set; payload: { values: Value[] } }
+  | { type: ActionType.Get };
+
+// In-line swap: https://stackoverflow.com/a/16201730/4035
+function swap(values: Value[], i1: number, i2: number): Value[] {
+  // log(`       swap Before => ${values}, i1=${i1}, i2=${i2}`);
+  // values[i2] = [values[i1], (values[i1] = values[i2])][0];
+  // log(`       swap AFTER => ${values}, i1=${i1}, i2=${i2}`);
+  //          0, 1, 2, 3, 4, 5, 6
+  // values=[10,20,30,40,50,60,70], i1=1 (20), i2=4(50) then
+  // [10], [?], [30, 40], [?], [60, 70]
+
+  const left = values.slice(0, i1);
+  const middle = values.slice(i1 + 1, i2);
+  const right = values.slice(i2 + 1);
+
+  // log(
+  //   `values=${values}, i1=${i1}, i2=${i2}, left, values[i2], middle, values[i1], right`,
+  //   left,
+  //   values[i2],
+  //   middle,
+  //   values[i1],
+  //   right
+  // );
+
+  return [...left, values[i2], ...middle, values[i1], ...right];
+}
+
+const getParentIndex = (childIndex: number): number => ~~((childIndex - 1) / 2);
+const hasParent = (childIndex: number): boolean =>
+  getParentIndex(childIndex) >= 0;
+const getParent = (values: Value[], childIndex: number): Value =>
+  values[getParentIndex(childIndex)];
+
+// type GetChildIndex = (parentIndex: number) => number;
+// const hasChild = (parentIndex: number, size: number): boolean => (
+//   getChildIndex: GetChildIndex
+// ) => getChildIndex(parentIndex) < size;
+
+const getRightChildIndex = (parentIndex: number) => parentIndex * 2 + 2;
+const hasRightChild = (parentIndex: number, size: number): boolean =>
+  getRightChildIndex(parentIndex) < size;
+const getRightChild = (values: Value[], parentIndex: number) =>
+  values[getRightChildIndex(parentIndex)];
+
+const getLeftChildIndex = (parentIndex: number) => parentIndex * 2 + 1;
+const hasLeftChild = (parentIndex: number, size: number): boolean =>
+  getLeftChildIndex(parentIndex) < size;
+const getLeftChild = (values: Value[], parentIndex: number) =>
+  values[getLeftChildIndex(parentIndex)];
+
+/**
+ * Heapify the last item
+ * @param {Value[]} values Values to re-heapify
+ * @returns {Value[]} A valid MinHeap
+ */
+function heapifyDown(values: Value[]): Value[] {
+  let index = 0;
+  let copy = [...values];
+  const size = copy.length;
+
+  while (hasLeftChild(index, copy.length)) {
+    let smallerChildIndex = getLeftChildIndex(index);
+    if (
+      hasRightChild(index, size) &&
+      getRightChild(copy, size) < getLeftChild(copy, size)
+    ) {
+      smallerChildIndex = getRightChildIndex(index);
+    }
+
+    if (copy[index] < copy[smallerChildIndex]) break;
+
+    copy = swap(copy, index, smallerChildIndex);
+    index = smallerChildIndex;
   }
-  return a + b;
-};
+
+  return copy;
+}
+
+function heapifyUp(values: Value[]) {
+  let heapedValues = [...values];
+  let index = heapedValues.length - 1;
+
+  while (
+    hasParent(index) &&
+    getParent(heapedValues, index) > heapedValues[index]
+  ) {
+    const parentIndex = getParentIndex(index);
+    heapedValues = swap(heapedValues, parentIndex, index);
+    index = parentIndex;
+  }
+  return heapedValues;
+}
+
+function addValue(values: Value[], value: Value) {
+  return heapifyUp([...values, value]);
+}
+
+function reducer(state: Value[], action: Action): Value[] {
+  switch (action.type) {
+    case ActionType.Add:
+      const addValues = addValue(state, action.payload.value);
+      return [...addValues];
+    case ActionType.Set:
+      const setValues = heapifyDown(action.payload.values);
+      return [...setValues];
+    case ActionType.Get:
+    default:
+      return state;
+  }
+}
+
+type HeapValue = Value | undefined;
+
+function useMinHeap(initialValues: Value[] = []) {
+  const [values, dispatch] = React.useReducer(
+    reducer,
+    initialValues,
+    initializer
+  );
+  const freshValues = React.useRef(values);
+  freshValues.current = values;
+
+  function initializer(values: Value[]): Value[] {
+    return values.reduce((acc, value) => addValue(acc, value), [] as Value[]);
+  }
+
+  function dump() {
+    return freshValues.current;
+  }
+
+  function add(value: Value) {
+    dispatch({ type: ActionType.Add, payload: { value } });
+  }
+
+  function get(): HeapValue {
+    const minimumValue = freshValues.current[0];
+
+    // We pop the first value and the current values is set to everything after the first item
+    dispatch({
+      type: ActionType.Set,
+      payload: { values: freshValues.current.slice(1) }
+    });
+
+    return minimumValue;
+  }
+
+  function peek(): HeapValue {
+    return freshValues.current[0];
+  }
+
+  return { dump, add, get, peek };
+}
+
+export { useMinHeap };
--- packages/useHeap/test/index.test.ts
+++ packages/useHeap/test/index.test.ts
@@ -1,7 +1,81 @@
-import { sum } from '../src';
+import { renderHook, act } from "react-hooks-testing-library";
+import { useMinHeap } from "../src";
 
-describe('fuck', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
+// const log = console.log;
+
+describe("useMinHeap", () => {
+  it("returns undefined when heap is empty", () => {
+    const { result } = renderHook(() => useMinHeap([]));
+    const heap = result.current;
+
+    act(() => expect(heap.get()).toBeUndefined());
+    act(() => expect(heap.peek()).toBeUndefined());
+  });
+
+  it("peeks correctly without removing any items", () => {
+    const input = [10, 2, 1, 99, 3];
+    const { result } = renderHook(() => useMinHeap(input));
+    const heap = result.current;
+
+    input.map(_ => expect(heap.peek()).toEqual(1));
+  });
+
+  it("gets minimum correctly", () => {
+    const { result } = renderHook(() => useMinHeap([]));
+    const heap = result.current;
+
+    const input = [10, 2, 1, 99, 3];
+    // ascending order
+    const expectedValues = [...input].sort((a, b) => a - b);
+
+    input.map(value => act(() => heap.add(value)));
+    expectedValues.map(expected =>
+      act(() => expect(heap.get()).toBe(expected))
+    );
+
+    // Nothing else to pop, so the result should be undefined
+    act(() => expect(heap.get()).toBeUndefined());
+  });
+
+  it("renders hook correctly", () => {
+    const { result } = renderHook(() => useMinHeap([]));
+    expect(result.current).not.toBeNull();
+  });
+
+  it("adds all correctly", () => {
+    const { result } = renderHook(() => useMinHeap([]));
+    const heap = result.current;
+
+    act(() => {
+      [9, 8, 6, 5, 3, 1].forEach(heap.add);
+    });
+
+    expect(heap.dump()).toEqual([1, 5, 3, 9, 6, 8]);
+  });
+
+  // prettier-ignore
+  it("adds correctly", () => {
+    const { result } = renderHook(() => useMinHeap([]));
+    const heap = result.current;
+
+    act(() => { heap.add(1); });
+    expect(heap.dump()).toEqual([1]);
+    act(() => { heap.add(3); });
+    expect(heap.dump()).toEqual([1, 3]);
+    act(() => { heap.add(5); });
+    expect(heap.dump()).toEqual([1, 3, 5]);
+    act(() => { heap.add(6); });
+    expect(heap.dump()).toEqual([1, 3, 5, 6]);
+    act(() => { heap.add(8); });
+    expect(heap.dump()).toEqual([1, 3, 5, 6, 8]);
+    act(() => { heap.add(9); });
+    expect(heap.dump()).toEqual([1, 3, 5, 6, 8, 9]);
+  });
+
+  it("dumps correctly", () => {
+    const values = [1, 3, 5, 6, 8, 9];
+    const { result } = renderHook(() => useMinHeap(values));
+
+    expect(result.current.dump()).toEqual([1, 3, 5, 6, 8, 9]);
   });
 });
